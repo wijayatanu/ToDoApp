7618ff32fe6f7b80546399899beb4805640c2602
Tanuwijaya Warkan <tanuwijaya.warkan@avanade.com>
2021-01-13 11:31:37 +0800

    first commit

---
+++ ARMTemplates/swap.ps1
@@ -0,0 +1,15 @@
+﻿Param(
+	[string] $Name,
+	[string] $FromSlot="staging"
+)
+
+if(!(Get-Module AzureRM))
+{
+	Import-Module AzureRM
+}
+
+#Swap backend first and then frontend
+Switch-AzureRmWebAppSlot -ResourceGroupName $Name -Name "${Name}Api" -SourceSlotName $FromSlot -DestinationSlotName production -Verbose
+Switch-AzureRmWebAppSlot -ResourceGroupName $Name -Name $Name -SourceSlotName $FromSlot -DestinationSlotName production -Verbose
+
+Write-Host "Done"
